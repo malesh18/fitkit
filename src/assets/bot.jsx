e4e626a915d6f4e15c70a8de226489cdb0a0557c
malesh18 <maleshchalwadi17@gmail.com>
2023-12-30 18:58:52 +0530

    update

---
+++ src/assets/bot.jsx
@@ -0,0 +1,60 @@
+import React, { useRef, useState, useEffect } from "react";
+// import download from "../Image/download.png";
+
+const Bot = () => {
+  const chatbotRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = (event) => {
+    if (chatbotRef.current && !chatbotRef.current.contains(event.target)) {
+      // Click outside the chatbot, close it
+      setIsOpen(false);
+    } else {
+      // Toggle chatbot visibility on icon click
+      setIsOpen(!isOpen);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={chatbotRef}
+      style={{
+        position: "fixed",
+        bottom: "20px",
+        right: "20px",
+        zIndex: "1000",
+        cursor: "pointer",
+      }}
+    >
+      <div onClick={handleClick}>
+        <img
+          src=""
+          alt="Excersise Bot"
+          style={{ width: "80px", height: "auto" }}
+        />
+      </div>
+      {isOpen && (
+        <iframe
+          src="https://webchat.botframework.com/embed/testingbotservices1-bot?s=6YW98xw4fic.toXCZkjzPEdVNf12Nro8XEg-Xghbe5_L2TVHb3VoPcU&style=dark"
+          style={{
+            border: "none",
+            display: "block",
+            minWidth: "400px",
+            width: "100%",
+            minHeight: "500px",
+          }}
+        ></iframe>
+      )}
+    </div>
+  );
+};
+
+export default Bot;
---
+++ src/excersie.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const SERVER_URI = "http://localhost:5000";
+
+const Excersie = () => {
+  const [eName, setEName] = useState("");
+  const [eRep, setERep] = useState("");
+  const [result, setResult] = useState([]);
+
+  const fetchResult = () => {
+    axios.get(`${SERVER_URI}/e/all`).then((res) => {
+      console.log("The Result :", res.data.result);
+      setResult(res.data.result);
+    }, console.error);
+  };
+
+  const handleSubmit = (e) => {
+    try {
+      e.preventDefault();
+      console.log(eName, eRep);
+
+      axios(`${SERVER_URI}/e/add`, {
+        method: "POST",
+        data: { eName, eRep },
+      }).then((res) => {
+        console.log("The Response : ", res.data);
+      }, console.error);
+    } catch (e) {
+      console.log("Error Occcured : ", e);
+    }
+  };
+
+  useEffect(() => {
+    fetchResult();
+  }, []);
+
+  return (
+    <>
+      <h1>Add the Excersie</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            value={eName}
+            onChange={(e) => setEName(e.target.value)}
+            type="text"
+            placeholder="excersies"
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            placeholder="Number of reps"
+            value={eRep}
+            onChange={(e) => setERep(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+
+      <div>
+        <h1>All the Excersies</h1>
+        <ul>
+          {result.map((item) => {
+            return (
+              <li key={item.id}>
+                {item.eName} - {item.eRep}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Excersie;
